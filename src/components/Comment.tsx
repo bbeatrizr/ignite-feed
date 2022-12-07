64e4d1e0be91c867fdbd3388ca150e6c009fa254
Beatriz Ramalho <biarl.ramalhorl@gmail.com>
2022-12-07 17:58:30 -0300

    style: 💄 finished project structure

---
+++ src/components/Comment.tsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/bbeatrizr.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Beatriz Ramalho</strong>
+              <time
+                title="23 de Novembro às 18:00h"
+                dateTime="2022-11-23 06:00:30 PM"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentario">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,5 @@
+import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -34,6 +36,21 @@ export function Post() {
           {""} <a>#nlw</a>
           {""} <a> #rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
